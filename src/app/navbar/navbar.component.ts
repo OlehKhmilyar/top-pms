fa377676b4480e26aff09176c0d3f3850aa0ea89
Oleh Khmilyar <olegkhmilyar@gmail.com>
2018-04-30 18:12:13 +0300

    Implement functionality for sending username from projects component to navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private rourter: Router) { }
 
   ngOnInit() {
+  }
+
+  @Input () userName: string = '';
+
+  logout(){
+    this.rourter.navigate(['/login']);
   }
 
 }
