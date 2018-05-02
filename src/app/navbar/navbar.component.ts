3fa810d6a1a6de58a9645ff156e7e3387cbe0a80
Oleh Khmilyar <olegkhmilyar@gmail.com>
2018-05-02 14:50:20 +0300

    Implement functionality for showing active navbar item in navbar component

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -11,7 +11,11 @@ export class NavbarComponent implements OnInit {
 
   constructor(private router: Router, private userService: UserService) { }
 
-  ngOnInit() {
+  url: String = null;
+
+  ngOnInit(
+  ) {
+    this.url = this.router.url;
   }
 
   logout(){
